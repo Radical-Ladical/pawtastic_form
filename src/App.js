2d9bbc8195b754fe08bbe87daa2f0ee8c8f4f87f
Student_M14 <student_m14@PS-DAL-M14.home>
2020-03-30 19:16:00 -0500

    8th commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ export default class app extends Component {
   render() {
     return(
       <div class ="Container">
-        <div class ="Row1">swag</div>
+        <div class ="Row1">
+          <ul class="list">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
         <div class =" Row2">swag</div>
       </div>
     )
