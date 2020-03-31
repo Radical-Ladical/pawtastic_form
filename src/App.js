85c124ca741a0602a439f00ef25f5a4b53631a16
Student_M14 <student_m14@PS-DAL-M14.home>
2020-03-30 21:15:13 -0500

    22nd commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default class app extends Component {
           <button class="back" onClick="">Back</button>
           <button class="next" onClick="">Next</button>
           <input type="checkbox" id="neut" class="box" />
-          <label for="toggle" class="neuter">blue</label>
+          <label for="neut" class="neuter">blue</label>
         </div>
       </div>
     )
